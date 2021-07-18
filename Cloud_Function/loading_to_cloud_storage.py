e6af28faa281fc0e203fd45e6f6245ab222b7bda
vijandrareddy <78939729+vijandrareddy@users.noreply.github.com>
2021-07-17 22:11:36 -0500

    Update loading_to_cloud_storage.py

--- Cloud_Function/loading_to_cloud_storage.py
+++ Cloud_Function/loading_to_cloud_storage.py
@@ -4,11 +4,7 @@ from google.cloud.storage import Client
 import datetime
 
 def hello_pubsub(event, context):
-    """Triggered from a message on a Cloud Pub/Sub topic.
-    Args:
-         event (dict): Event payload.
-         context (google.cloud.functions.Context): Metadata for the event.
-    """
+
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
     print(pubsub_message)
     df = pandas.read_json(pubsub_message)
@@ -18,6 +14,6 @@ def hello_pubsub(event, context):
     current_date_time = datetime.datetime.now()
     current_date_time_formatted = current_date_time.strftime("%m-%d-%Y-%H%M%S")
     storage_client = Client()
-    bucket = storage_client.get_bucket("project-covid")   
-    blob=bucket.blob("raw_data/parking_citation " + current_date_time_formatted + ".csv")
-    blob.upload_from_string(data=df.to_csv(index=False),content_type="text/csv") 
+    bucket = storage_client.get_bucket("project_egen")   
+    blob=bucket.blob("new_data/parking_citation " + current_date_time_formatted + ".csv")
+    blob.upload_from_string(data=df.to_csv(index=False),content_type="text/csv")    
